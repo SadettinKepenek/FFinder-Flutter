a63539751803b7b7c790faa5985767a13377944f
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-21 00:02:57 +0300

    sadasd

---
+++ lib/screens/MainPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MainPageState();
+}
+
+class MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Column(
+        children: <Widget>[Text("Test")],
+      ),
+    );
+  }
+}
