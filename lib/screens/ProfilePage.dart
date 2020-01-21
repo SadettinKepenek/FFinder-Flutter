1cf51d1a19638e5dc9440f875ec89402837715f8
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-22 01:33:28 +0300

    Profile Page Methodlar Module yapıldı

--- lib/screens/ProfilePage.dart
+++ lib/screens/ProfilePage.dart
@@ -1,3 +1,4 @@
+import 'package:ffinder/models/Post_DataTransferObjects/PostDetailDto.dart';
 import 'package:ffinder/models/User_DataTransferObjects/UserDetailDto.dart';
 import 'package:ffinder/services/ApiService.dart';
 import 'package:flutter/cupertino.dart';
@@ -61,7 +62,7 @@ class ProfilePageState extends State<ProfilePage> {
           child: Card(
             child: Column(
               children: <Widget>[
-                _buildProfilePhoto(),
+                _buildProfile(),
               ],
             ),
           ),
@@ -71,75 +72,218 @@ class ProfilePageState extends State<ProfilePage> {
   }
 
   _buildProfilePhoto() {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          backgroundImage: NetworkImage(profileDto.profilePhotoUrl),
+          radius: 50,
+          backgroundColor: Colors.transparent,
+        ),
+        SizedBox(
+          height: 0.1,
+          width: 10,
+        ),
+      ],
+    );
+  }
+
+  _buildProfileInfo() {
+    return SizedBox(
+      height: 110,
+      width: MediaQuery.of(context).size.width - 150,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("Gönderi Sayısı"),
+              Text("Takipçi"),
+              Text("Takip Edilen"),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("${profileDto.post.length}"),
+              Text("${profileDto.follower.length}"),
+              Text("0"),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Takip Et"),
+                onPressed: () {},
+                elevation: 10,
+              ),
+              SizedBox(
+                width: 2,
+              ),
+              RaisedButton(
+                child: Text("Mesaj"),
+                onPressed: () {},
+                elevation: 10,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildProfile() {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            CircleAvatar(
-              backgroundImage: NetworkImage(profileDto.profilePhotoUrl),
-              radius: 50,
-              backgroundColor: Colors.transparent,
-            ),
-            SizedBox(
-              height: 0.1,
-              width: 10,
-            ),
-          ],
-        ),
-        SizedBox(
-          height: 110,
-          width: MediaQuery.of(context).size.width - 150,
-          child: Column(
+      children: <Widget>[_buildProfilePhoto(), _buildProfileInfo()],
+    );
+  }
+
+  _buildPostImageTop() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(5, 0, 8, 5),
+      child: Row(
+        children: <Widget>[
+          Column(
             children: <Widget>[
-              SizedBox(
-                height: 20,
-              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Text("Gönderi Sayısı"),
-                  Text("Takipçi"),
-                  Text("Takip Edilen"),
-                ],
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text("${profileDto.post.length}"),
-                  Text("${profileDto.follower.length}"),
-                  Text("0"),
-                ],
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    child: Text("Takip Et"),
-                    onPressed: () {},
-                    elevation: 10,
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(profileDto.profilePhotoUrl),
+                    radius: 15,
+                    backgroundColor: Colors.transparent,
                   ),
                   SizedBox(
-                    width: 2,
+                    width: 5,
                   ),
-                  RaisedButton(
-                    child: Text("Mesaj"),
-                    onPressed: () {},
-                    elevation: 10,
-                  )
+                  Text(
+                    "berkayalcin",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
                 ],
               )
             ],
           ),
-        )
+        ],
+      ),
+    );
+  }
+
+  _buildPostBody(post) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: .5),
+      child: Image.network(
+        post.postImageUrl,
+        height: 280,
+        cacheHeight: 280,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.fill,
+      ),
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(width: .5, color: Colors.blueGrey),
+              top: BorderSide(width: .5, color: Colors.blueGrey),
+              left: BorderSide(width: .5, color: Colors.blueGrey),
+              right: BorderSide(width: .5, color: Colors.blueGrey)),
+          shape: BoxShape.rectangle),
+    );
+  }
+
+  _buildPostActionButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.mood),
+          iconSize: 24,
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.mood_bad),
+          onPressed: () {},
+          iconSize: 24,
+        ),
+        IconButton(
+          icon: Icon(Icons.comment),
+          onPressed: () {},
+          iconSize: 24,
+        ),
+        IconButton(
+          icon: Icon(Icons.share),
+          onPressed: () {},
+          iconSize: 24,
+        ),
       ],
+    );
+  }
+
+  _buildPostLikes(PostDetailDto post) {
+    return Padding(
+      child: Row(
+        children: <Widget>[
+          Text(
+            "${post.rates.where((data) => data.isLike).toList().length} Likes",
+            style: TextStyle(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 0.1,
+            width: 5,
+          ),
+          Text(
+            "${post.rates.where((data) => data.isLike == false).toList().length} Dislikes",
+            style: TextStyle(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      padding: EdgeInsets.fromLTRB(10, 0, 10, 2),
+    );
+  }
+
+  _buildPostInfo(PostDetailDto post) {
+    return Padding(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          RichText(
+            maxLines: 3,
+            text: TextSpan(children: [
+              TextSpan(
+                text: post.owner.userName,
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              TextSpan(text: " "),
+              TextSpan(
+                text: post.postBody,
+                style: DefaultTextStyle.of(context).style,
+              )
+            ]),
+          ),
+          SizedBox(
+            height: .5,
+          ),
+          Text(
+            timeago.format(post.publishDate),
+            maxLines: 1,
+            style: TextStyle(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
     );
   }
 
@@ -152,131 +296,11 @@ class ProfilePageState extends State<ProfilePage> {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(5, 0, 8, 5),
-            child: Row(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage:
-                              NetworkImage(profileDto.profilePhotoUrl),
-                          radius: 15,
-                          backgroundColor: Colors.transparent,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "berkayalcin",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: .5),
-            child: Image.network(
-              post.postImageUrl,
-              height: 280,
-              cacheHeight: 280,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.fill,
-            ),
-            decoration: BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(width: .5, color: Colors.blueGrey),
-                    top: BorderSide(width: .5, color: Colors.blueGrey),
-                    left: BorderSide(width: .5, color: Colors.blueGrey),
-                    right: BorderSide(width: .5, color: Colors.blueGrey)),
-                shape: BoxShape.rectangle),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.mood),
-                iconSize: 24,
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.mood_bad),
-                onPressed: () {},
-                iconSize: 24,
-              ),
-              IconButton(
-                icon: Icon(Icons.comment),
-                onPressed: () {},
-                iconSize: 24,
-              ),
-              IconButton(
-                icon: Icon(Icons.share),
-                onPressed: () {},
-                iconSize: 24,
-              ),
-            ],
-          ),
-          Padding(
-            child: Row(
-              children: <Widget>[
-                Text(
-                  "${post.rates.where((data) => data.isLike).toList().length} Likes",
-                  style: TextStyle(
-                      fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 0.1,
-                  width: 5,
-                ),
-                Text(
-                  "${post.rates.where((data) => data.isLike == false).toList().length} Dislikes",
-                  style: TextStyle(
-                      fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            padding: EdgeInsets.fromLTRB(10, 0, 10, 2),
-          ),
-          Padding(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                RichText(
-                  maxLines: 3,
-                  text: TextSpan(children: [
-                    TextSpan(
-                      text: post.owner.userName,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                    TextSpan(text: " "),
-                    TextSpan(
-                      text: post.postBody,
-                      style: DefaultTextStyle.of(context).style,
-                    )
-                  ]),
-                ),
-                SizedBox(
-                  height: .5,
-                ),
-                Text(
-                  timeago.format(post.publishDate),
-                  maxLines: 1,
-                  style: TextStyle(
-                      fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-          ),
+          _buildPostImageTop(),
+          _buildPostBody(post),
+          _buildPostActionButtons(),
+          _buildPostLikes(post),
+          _buildPostInfo(post),
           Divider(
             color: Theme.of(context).primaryColor,
             thickness: 0.35,
