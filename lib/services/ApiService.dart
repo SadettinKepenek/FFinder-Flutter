0e2cf3208b657793d59745119e745b69375bbc66
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-21 16:42:30 +0300

    Add GetMyProfile method

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:ffinder/models/User_DataTransferObjects/UserDetailDto.dart';
 import 'package:ffinder/models/User_DataTransferObjects/UserLoginRequestDto.dart';
 import 'package:ffinder/models/User_DataTransferObjects/UserLoginResponseDto.dart';
 import 'package:http/http.dart';
@@ -23,6 +24,23 @@ class ApiService {
       await StorageService.initAuth(loginResponseDto);
 
       return loginResponseDto;
+    }
+    return null;
+  }
+
+  static Future<UserDetailDto> getMyProfile() async {
+    String url = "";
+    Map<String, String> headers = {
+      "Content-type": "application/json",
+      "Authorization":"Beraer ${(await StorageService.getAuth()).token}"
+    };
+    Response response = await get(url, headers: headers);
+    int statusCode = response.statusCode;
+    String responseBody = response.body;
+    Map<String, dynamic> json = jsonDecode(responseBody);
+    if(statusCode==200){
+      UserDetailDto dto=UserDetailDto.fromJson(json["data"]);
+      return dto;
     }
     return null;
   }
